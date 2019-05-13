719f3b595bdcfe19b1f58d01e71f15c052a33942
Kelvin <kfconsultant@gmail.com>
2019-05-13 15:52:27 -0600

    Updates

---
+++ precache-manifest.9589009c75c130fe3c56e0706aafc063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "936c34daa7d5ca26bfb7",
+    "url": "/contactmanager/static/js/main.58f91e66.chunk.js"
+  },
+  {
+    "revision": "c1e39d7388dcdfe1959b",
+    "url": "/contactmanager/static/js/2.21bc4830.chunk.js"
+  },
+  {
+    "revision": "936c34daa7d5ca26bfb7",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "c1e39d7388dcdfe1959b",
+    "url": "/contactmanager/static/css/2.7c5b8dca.chunk.css"
+  },
+  {
+    "revision": "54c4e55600cc3e46e233e00d78234fe5",
+    "url": "/contactmanager/index.html"
+  }
+];
